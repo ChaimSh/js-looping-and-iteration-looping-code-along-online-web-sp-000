42c3fc4b13f86772e95d5c35f2069618c8c84d8d
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-03 01:15:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
-function writeCards(namesArray, event){
-  for (let i = 0, i < array.lenght; i++) {
+function writeCards(namesArray, event) {
+  for (let i = 0, i < namesArray.lenght; i++) {
     return "Thank you, ${array[i]} for the wonderful birhtday gift!"
   }
 }
