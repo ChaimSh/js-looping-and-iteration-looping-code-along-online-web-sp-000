79453ef555ab46db5fa3a887bfa0d30506f2c14b
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-03 01:31:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ function writeCards(namesArray, event) {
   return thankYouCards
 }
 
-// function writeCards( namesArray, event ) {
-//   let thankYouCards = []
-//   for ( let i = 0; i < namesArray.length; i++ ) {
-//     thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
-//   }
-//   return thankYouCards
-// }
-
 
  function countdown( countdown ) {
-
+   while (countdown > 0) {
+     return countdown
+   }
  }
