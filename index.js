0fbc1232d6ddc72f342c9271c8c51f147c6ef968
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-01-03 01:17:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function writeCards(namesArray, event) {
   for (let i = 0, i < namesArray.length; i++) {
     thankYouCards.push(`Thank you, ${namesArray[i]} for the wonderful ${event} gift!`)
   }
+  return thankYouCards
 }
